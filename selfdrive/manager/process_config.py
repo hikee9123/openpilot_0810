7568adbb666703c8e4818ea6e366de2c45e7707c
hikee9123 <hikee9123@gmail.com>
2021-10-16 22:40:42 +0900

    osm

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -36,6 +36,8 @@ procs = [
   PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
   PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
 
+  PythonProcess("mapd", "selfdrive.mapd.mapd"),
+
   # EON only
   PythonProcess("rtshield", "selfdrive.rtshield", enabled=EON),
   PythonProcess("androidd", "selfdrive.hardware.eon.androidd", enabled=EON, persistent=True),
